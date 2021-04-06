06d3b206f8e597a9dde79bd5e9abf36c7a3e9c0c
Ethan Fox <efox@solutioninnovators.com>
2021-04-05 21:14:24 -0400

    Initial Commit

---
+++ Pop Fox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pop Fox
+//
+//  Created by Ethan Fox on 4/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
